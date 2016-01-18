f1b8d35f6e1470fc4f43258c4f52931375093fc1
Nick Ramirez <nramirez@aclens.local>
2016-01-18 17:19:18 -0500

    Fixes #2 and #3 - iis_errorpages throws various errors.

--- lib/puppet/provider/iis_errorpages/iis_errorpages.rb
+++ lib/puppet/provider/iis_errorpages/iis_errorpages.rb
@@ -65,16 +65,27 @@ Puppet::Type.type(:iis_errorpages).provide(:iis_errorpages, :parent => Puppet::P
     hash
   end
 
-  def get_complex_property_arg(name, value)
+  def get_complex_property_arg(name, error_pages_to_add)
     args = nil
 
     case name
     when :error_pages
-      to_remove = @initial_properties[:error_pages].reject{|x| if value.include?(x); x end}
-      to_add = value.reject{|x| if @initial_properties[:error_pages].include?(x); x end}
+	  stringify_statuscode!(error_pages_to_add)
+	  strinfiy_substatuscode!(error_pages_to_add)
+	  stringify_statuscode!(@initial_properties[:error_pages])
+	  strinfiy_substatuscode!(@initial_properties[:error_pages])
+	  
+	  to_remove = @initial_properties[:error_pages].select do |existing_error_page| 
+		error_page_for_statuscode_exists = found_error_page_with_same_statuscode_as_we_are_adding(error_pages_to_add, existing_error_page)
+		existing_error_page_not_same = !error_pages_to_add.include?(existing_error_page)
+		
+		error_page_for_statuscode_exists && existing_error_page_not_same
+	  end
+
+      to_add = error_pages_to_add.reject{|x| if @initial_properties[:error_pages].include?(x); x end}
 
       to_remove.map do |page|
-        (args ||= []) << "\"/-[" + page.map{|k,v| "#{k}='#{v}'" if k =~ /statusCode/i}.join(',') + "]\""
+        (args ||= []) << "\"/-[" + page.map{|k,v| "#{k}='#{v}'" if k =~ /statusCode/i}.join('') + "]\""
       end unless to_remove.empty?
 
       to_add.map do |page|
@@ -83,6 +94,12 @@ Puppet::Type.type(:iis_errorpages).provide(:iis_errorpages, :parent => Puppet::P
     end
 
     args
+  end
+  
+  # On subsequent runs, after the custom error pages have been added, 
+  # get_complex_property_arg will return an empty array and this method 
+  # will be called by the parent class. In that case, it should do nothing.
+  def get_simple_property_arg(name, error_pages_to_add)
   end
 
   def execute_flush
@@ -93,5 +110,41 @@ Puppet::Type.type(:iis_errorpages).provide(:iis_errorpages, :parent => Puppet::P
  def execute_delete
    appcmd 'clear', 'config', @resource[:name], "/section:system.webServer/httpErrors"
  end
+ 
+ #-----------Private Methods-----------------
+  private
+  # The statusCode can come over as an integer. Convert it to a string 
+  # so that when comparing, we know we're comparing apples to apples.
+  # [error_pages] The array of hashes where each hash is the custom error page
+  def stringify_statuscode!(error_pages)
+    return if error_pages.nil?
+	error_pages.each { |error_page| error_page['statusCode'] = error_page['statusCode'].to_s }
+  end
+  
+  # The subStatusCode can come over as an integer. Convert it to a string 
+  # so that when comparing, we know we're comparing apples to apples.
+  # [error_pages] The array of hashes where each hash is the custom error page
+  def strinfiy_substatuscode!(error_pages)
+    return if error_pages.nil?
+    error_pages.each do |error_page| 
+	  if !error_page['subStatusCode'].nil?
+	    error_page['subStatusCode'] = error_page['subStatusCode'].to_s 
+	  end
+	end
+  end
+  
+  # Gets a value indicating whether there is an existing custom error page with the same 
+  # statusCode and subStatusCode as the one we're trying to add
+  # [error_pages_to_add] Array of error page hashes to add
+  # [existing_error_page] Existing error page to compare to
+  def found_error_page_with_same_statuscode_as_we_are_adding(error_pages_to_add, existing_error_page)
+    error_pages_to_add.any? do |error_page| 
+	  if (!error_page['subStatusCode'].nil?)
+	    error_page['statusCode'] == existing_error_page['statusCode'] && error_page['subStatusCode'] == existing_error_page['subStatusCode']
+	  else
+	    error_page['statusCode'] == existing_error_page['statusCode'] 
+	  end
+	end
+  end
 
 end
